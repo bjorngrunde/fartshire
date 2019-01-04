fb05c52818761fe68a0ac61e0e20135e3a2a6141
Bjorn Grunde <bjorngrunde@live.se>
2019-01-04 21:33:16 +0100

    Initial Commit

---
+++ Source/fartshire/fartshire.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "fartshire.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, fartshire, "fartshire" );
---
+++ Source/fartshire/fartshireGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "fartshireGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARTSHIRE_API AfartshireGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
